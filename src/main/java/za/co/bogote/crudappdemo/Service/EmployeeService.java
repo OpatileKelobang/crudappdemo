c221cbc2da4b56a754187ab79edbd0e5b64c1fea
OpatileKelobang <kelobang.opatile@gmail.com>
2020-09-09 13:20:21 +0200

    Service

--- src/main/java/za/co/bogote/crudappdemo/Service/EmployeeService.java
+++ src/main/java/za/co/bogote/crudappdemo/Service/EmployeeService.java
@@ -1,4 +1,12 @@
 package za.co.bogote.crudappdemo.Service;
 
+import za.co.bogote.crudappdemo.Modal.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> get();
+    Employee get(int id);
+    void save(Employee employee);
+    void delete(int id);
 }
